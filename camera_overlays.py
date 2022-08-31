7ed2e8f0db0c3156615430c9a4c2c4a49632c0cb
Don Schniitzius <don@sketchbookclub.com>
2022-08-31 04:08:51 -0500

    camera-overlays v.1.1.0
    Add Passeport Toggle

--- camera_overlays.py
+++ camera_overlays.py
@@ -2,11 +2,11 @@ bl_info = {
     "name": "Camera Overlays",
     "description": "Show/Hide Harmony and Golden Ratios and Triangles",
     "author": "Don Schnitzius",
-    "version": (1, 0),
-    "blender": (3, 0, 0),
+    "version": (1, 1, 0),
+    "blender": (2, 80, 0),
     "location": "Context Menu (Right Mouse Button)",
     "warning": "",
-    "doc_url": "",
+    "doc_url"   : "https://github.com/don1138/blender-qle",
     "tracker_url": "",
     "support": "COMMUNITY",
     "category": "Camera",
@@ -16,44 +16,19 @@ bl_info = {
 import bpy
 
 
-class CAMERA_SCT_overlays(bpy.types.Operator):
-    """Show/Hide Harmony Overlays"""
-    bl_idname = "camera.harmony_overlays"
-    bl_label = "Show Harmony Overlays"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    @classmethod
-    def poll(cls, context):
-        ob = bpy.context.active_object
-        return ob and ob.type == 'CAMERA'
-
-    def execute(self, context):
-        ob = bpy.context.object.data
-        if ob.show_composition_thirds == False:
-            ob.show_composition_thirds = True
-            ob.show_composition_harmony_tri_a = True
-            ob.show_composition_harmony_tri_b = True
-        else:
-            ob.show_composition_thirds = False
-            ob.show_composition_harmony_tri_a = False
-            ob.show_composition_harmony_tri_b = False
-
-        return {'FINISHED'}
-
-
 class CAMERA_SCG_overlays(bpy.types.Operator):
     """Show/Hide Golden Overlays"""
     bl_idname = "camera.golden_overlays"
     bl_label = "Show Golden Overlays"
     bl_options = {'REGISTER', 'UNDO'}
 
-    @classmethod
-    def poll(cls, context):
-        ob = bpy.context.active_object
-        return ob and ob.type == 'CAMERA'
+#    @classmethod
+#    def poll(cls, context):
+#        ob = bpy.context.active_object
+#        return ob and ob.type == 'CAMERA'
 
     def execute(self, context):
-        ob = bpy.context.object.data
+        ob = bpy.context.scene.camera.data
 
         if ob.show_composition_golden == False:
             ob.show_composition_golden = True
@@ -67,16 +42,67 @@ class CAMERA_SCG_overlays(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class CAMERA_SCT_overlays(bpy.types.Operator):
+    """Show/Hide Harmony Overlays"""
+    bl_idname = "camera.harmony_overlays"
+    bl_label = "Show Harmony Overlays"
+    bl_options = {'REGISTER', 'UNDO'}
+
+#    @classmethod
+#    def poll(cls, context):
+#        ob = bpy.context.active_object
+#        return ob and ob.type == 'CAMERA'
+
+    def execute(self, context):
+        ob = bpy.context.scene.camera.data
+
+        if ob.show_composition_thirds == False:
+            ob.show_composition_thirds = True
+            ob.show_composition_harmony_tri_a = True
+            ob.show_composition_harmony_tri_b = True
+        else:
+            ob.show_composition_thirds = False
+            ob.show_composition_harmony_tri_a = False
+            ob.show_composition_harmony_tri_b = False
+
+        return {'FINISHED'}
+
+
+class CAMERA_SP_passeport(bpy.types.Operator):
+    """Set Passeport to 1.0"""
+    bl_idname = "camera.passeport_toggle"
+    bl_label = "Set Passeport to 1.0"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    # @classmethod
+    # def poll(cls, context):
+    #     ob = bpy.context.active_object
+    #     return ob and ob.type == 'CAMERA'
+
+    def execute(self, context):
+        ob = bpy.context.scene.camera.data
+
+        if ob.passepartout_alpha != 1:
+            ob.passepartout_alpha = 1
+        else:
+            ob.passepartout_alpha = 0.5
+
+        return {'FINISHED'}
+
+
 classes = [
-    CAMERA_SCT_overlays,
     CAMERA_SCG_overlays,
+    CAMERA_SCT_overlays,
+    CAMERA_SP_passeport,
 ]
 
 
 def draw_inmenu(self, context):
     self.layout.separator()
-    self.layout.operator("camera.harmony_overlays", text="Harmony Overlays")
     self.layout.operator("camera.golden_overlays", text="Golden Overlays")
+    self.layout.operator("camera.harmony_overlays", text="Harmony Overlays")
+    self.layout.operator("camera.passeport_toggle", text="Passeport Toggle")
+    self.layout.separator()
 
 
 def register():
